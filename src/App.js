bf94ee5cfe6e6582c9acbf50ae93ee84597b4148
guandjoy <guandjoy@gmail.com>
2021-07-24 17:45:51 +0300

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Shaders from "./views/Shaders";
 import Shader from "./views/Shader";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route path="/" exact component={Shaders} />
         <Route path="/shaders/:id" component={Shader} />
